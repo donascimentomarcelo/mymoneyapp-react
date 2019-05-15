92bab5b10b39d6220a510013c5b662c7c06890ee
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2019-05-14 21:46:09 -0300

    Creating component footer.

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2017
+            <a href='#'>Site</a>
+        </strong>
+    </footer>
+
+);
