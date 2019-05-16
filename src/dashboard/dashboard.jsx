361117121a4bd7f18f93e46c26bd6b78cc10e3f5
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2019-05-15 22:36:21 -0300

    Creating the component of title content.

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default (props) => (
+import ContentHeader from '../common/template/contentHeader';
+import Content from '../common/template/content';
 
-    <div>
-        <h1>Dashboard</h1>
-    </div>
-);
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Dashboard' small='Versão 1.0'>
+                    <Content>
+                    Dashboard
+                    </Content>
+                </ContentHeader>
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
