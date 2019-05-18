6fc5759c3bf9f932c55ab1ff7c72bd654f7dbd78
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2019-05-17 23:37:08 -0300

    Fixing dashboard.

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -9,30 +9,29 @@ class Dashboard extends Component {
     render() {
         return (
             <div>
-                <ContentHeader title='Dashboard' small='Versão 1.0'>
-                    <Content>
-                        <Row>
-                            <ValueBox 
-                            cols='12 4' 
-                            color='green' 
-                            icon='bank'
-                            value='R$ 10'
-                            text='Total de Créditos'/>
-                            <ValueBox 
-                            cols='12 4' 
-                            color='red' 
-                            icon='credit-card'
-                            value='R$ 10'
-                            text='Total de Débitos'/>
-                            <ValueBox 
-                            cols='12 4' 
-                            color='blue' 
-                            icon='money'
-                            value='R$ 10'
-                            text='Valor Consolidado'/>
-                        </Row>
-                    </Content>
-                </ContentHeader>
+                <ContentHeader title='Dashboard' small='Versão 1.0'/>
+                <Content>
+                    <Row>
+                        <ValueBox 
+                        cols='12 4' 
+                        color='green' 
+                        icon='bank'
+                        value='R$ 10'
+                        text='Total de Créditos'/>
+                        <ValueBox 
+                        cols='12 4' 
+                        color='red' 
+                        icon='credit-card'
+                        value='R$ 10'
+                        text='Total de Débitos'/>
+                        <ValueBox 
+                        cols='12 4' 
+                        color='blue' 
+                        icon='money'
+                        value='R$ 10'
+                        text='Valor Consolidado'/>
+                    </Row>
+                </Content>
             </div>
         );
     }
