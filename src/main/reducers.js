838df9b88adc7d51d29e7afbaf212dc2c8a436e5
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2019-05-17 23:48:36 -0300

    Configuring redux.

---
+++ src/main/reducers.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+    dashboard: () => ({
+        sumary: {
+            credit: 100,
+            debit: 50
+        }
+    })
+});
+
+export default rootReducer
